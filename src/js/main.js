d18eb45d6426b105e0ac32c46388ca2de28ed534
Rosa Ponce Espín <yosoyrosaponce@gmail.com>
2024-01-19 13:09:06 +0100

    renderAnime update

--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,67 @@
 'use strict';
 
-import '/search.js';
+import './consts.js';
 
+console.log('hola');
+
+/* let resultContainer = document.querySelector('.js-allAnimes'); */
 
 let resultAnimes = [];
 let favAnimes = [];
-const url = "https://api.jikan.moe/v4/anime"
-let resultContainer = document.querySelector('.js-allAnimes');
+const url = "https://api.jikan.moe/v4/anime?q="
+
+
+
+
+
 
 function getDataApi() {
     fetch(url)
     .then(response => response.json())
     .then(data => {
         resultAnimes = data.data;
-        console.log(resultAnimes);
-        renderAnime();
+        console.log('jajaja');
+       
 
-        //aquí irá luego la función render y lo de meterlo en el localstorage  
+        //aquí irá luego lo de meterlo en el localstorage  
     })
 
 }
+inputSearch.addEventListener('click', getDataApi, renderAnime);
 
-getDataApi();
 
 function renderAnime() {
+
+    
     let inputSearchValue = inputSearch.value;
     
-    resultContainer.innerHTML = "";
-
+    
     let userSearch = resultAnimes.filter((item) => item.title.toLowerCase().includes(inputSearchValue.toLowerCase()));
-    
+    const noImageImage = "https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg"
     for (const eachAnime of userSearch) {
+        let resultContainer = document.querySelector('.js-allAnimes');
+        resultContainer.innerHTML = "";
     
-        resultContainer.innerHTML += ` <div class="js-allAnimes">
-        <div class="js-animeCard">
+        if (eachAnime.images.jpg.image_url === null) {
+
+        resultContainer.innerHTML += ` 
+        <li class="js-animeCard" id=${eachAnime.mal_id}>
+            <h3 class="js-animeTitle">${eachAnime.title}</h3>
+            <img src="${noImageImage}" alt="cartel de ${eachAnime.title}" class="js-animeImage">
+        </li>`
+
+        } else {
+    
+        resultContainer.innerHTML += `
+        <li class="js-animeCard" id=${eachAnime.mal_id}>
             <h3 class="js-animeTitle">${eachAnime.title}</h3>
             <img src="${eachAnime.images.jpg.image_url}" alt="cartel de ${eachAnime.title}" class="js-animeImage">
-        </div>
-    </div>`
+        </li>`
     }
-   
+}
     
 };
             
-        
 
-
-inputSearch.addEventListener('input', renderAnime);
+btnSearch.addEventListener('click',renderAnime)
+/* inputSearch.addEventListener('input', renderAnime); */
