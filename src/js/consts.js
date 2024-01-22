428d73cbd53c8e3abb63db72ed25f74132c4d6a5
Rosa Ponce Espín <yosoyrosaponce@gmail.com>
2024-01-22 08:38:12 +0100

    getDataLocal

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,10 +1,9 @@
-const h1 = document.createElement('h3');
+const h1 = document.createElement('h1');
 const h1Title = document.createTextNode('Buscador de series de anime');
 h1.appendChild(h1Title);
-
 const searchSection = document.querySelector('.js-search-section');
-
 searchSection.appendChild(h1);
+
 
 const inputSearch = document.createElement('input')
 inputSearch.type = 'text';
@@ -13,13 +12,30 @@ searchSection.appendChild(inputSearch);
 const btnSearch = document.createElement('button');
 const btnSearchText = document.createTextNode('Buscar');
 btnSearch.appendChild(btnSearchText);
-
 searchSection.appendChild(btnSearch);
-
 const btnReset = document.createElement('button');
 const btnResetText = document.createTextNode('Reset');
 btnReset.appendChild(btnResetText);
-
 searchSection.appendChild(btnReset);
+
+
+/* const h2Favs = document.createElement('h2');
+const h2Favstitle = document.createTextNode('Series favoritas');
+h2Favs.appendChild(h2Favstitle);
+const favSection = document.querySelector('.js-fav-section');
+favSection.appendChild(h2Favs);
+const favsContainer = document.querySelector('js-favsList');
+
+const h2Results = document.createElement('h2');
+const h2ResultsText = document.createTextNode('Resultados');
+h2Results.appendChild(h2ResultsText);
+const resultsSection = document.querySelector('.js-results-section');
+resultsSection.appendChild(h2Results); */
+
+
+
+
+
+
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,15 @@
 'use strict';
 
+
 import './consts.js';
 
-console.log('hola');
-
-/* let resultContainer = document.querySelector('.js-allAnimes'); */
 
 let resultAnimes = [];
 let favAnimes = [];
 const url = "https://api.jikan.moe/v4/anime?q="
+let resultContainer = document.querySelector('.js-animeList');
 
-
-
-
+const favsContainer = document.querySelector('.js-favsList');
 
 
 function getDataApi() {
@@ -20,33 +17,91 @@ function getDataApi() {
     .then(response => response.json())
     .then(data => {
         resultAnimes = data.data;
-        console.log('jajaja');
+        renderAnime();
        
-
-        //aquí irá luego lo de meterlo en el localstorage  
+        
     })
 
 }
-inputSearch.addEventListener('click', getDataApi, renderAnime);
+
+getDataLocal();
+
+function handleFavAnimes(event) {
+    event.preventDefault();
+    
+    const idAnimeClicked = event.currentTarget.id;
+
+    const foundAnime = resultAnimes.find((anime) =>
+     idAnimeClicked == anime.mal_id);
+     
+     
+   const indexAnimesInFav = favAnimes.findIndex((anime) => anime.mal_id === idAnimeClicked);
+
+    if(indexAnimesInFav === -1) {
+        favAnimes.push(foundAnime);
+       localStorage.setItem('Series favoritas', JSON.stringify(favAnimes));
+
+       
+    }   
+   
+     renderFav() 
+
+
+   
+    
+}
+
+
+ 
+function getDataLocal() {
+    let localAnimes = localStorage.getItem('Series favoritas');
+    localAnimes = JSON.parse(localAnimes);
+    for (const eachLocal of localAnimes) {
+        favsContainer.innerHTML += ` 
+        <li  class="js-animeCard favs" id=${eachLocal.mal_id}>
+            <h3 class="js-animeTitle">${eachLocal.title}</h3><i class="fa-regular fa-circle-xmark"></i>
+            <img src="${eachLocal.images.jpg.image_url}" alt="cartel de ${eachLocal.title}" class="js-animeImage">
+        </li>`
+    }
+
+}
+
+
+
+
+
+
+
+function listenerAnimes() {
+
+    const allAnimes = document.querySelectorAll('.js-animeCard');
+
+    for (const oneAnime of allAnimes) {
+        oneAnime.addEventListener('click', handleFavAnimes);
+        oneAnime.classList.add('favourite');
+        
+       
+    }
+    
+}
 
 
 function renderAnime() {
-
-    
+  
+    resultContainer.innerHTML = "";
     let inputSearchValue = inputSearch.value;
-    
-    
+
     let userSearch = resultAnimes.filter((item) => item.title.toLowerCase().includes(inputSearchValue.toLowerCase()));
+    
     const noImageImage = "https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg"
+
     for (const eachAnime of userSearch) {
-        let resultContainer = document.querySelector('.js-allAnimes');
-        resultContainer.innerHTML = "";
     
         if (eachAnime.images.jpg.image_url === null) {
 
         resultContainer.innerHTML += ` 
-        <li class="js-animeCard" id=${eachAnime.mal_id}>
-            <h3 class="js-animeTitle">${eachAnime.title}</h3>
+        <li  class="js-animeCard" id=${eachAnime.mal_id}>
+            <h3 class="js-animeTitle">${eachAnime.title}</h3><i class="fa-regular fa-circle-xmark"></i>
             <img src="${noImageImage}" alt="cartel de ${eachAnime.title}" class="js-animeImage">
         </li>`
 
@@ -54,14 +109,45 @@ function renderAnime() {
     
         resultContainer.innerHTML += `
         <li class="js-animeCard" id=${eachAnime.mal_id}>
-            <h3 class="js-animeTitle">${eachAnime.title}</h3>
+            <h3 class="js-animeTitle">${eachAnime.title}</h3><i class="fa-regular fa-circle-xmark"></i>
             <img src="${eachAnime.images.jpg.image_url}" alt="cartel de ${eachAnime.title}" class="js-animeImage">
         </li>`
     }
+
+
 }
+listenerAnimes()
     
 };
-            
 
-btnSearch.addEventListener('click',renderAnime)
-/* inputSearch.addEventListener('input', renderAnime); */
+
+const renderFav = () => {
+    for (const eachFav of favAnimes) {
+        
+    
+        const renderedFavAnimes = favsContainer.querySelector(`[id="${eachFav.mal_id}"]`);
+
+        if (!renderedFavAnimes) {
+            favsContainer.innerHTML += `<li class="js-animeCard favs" id=${eachFav.mal_id}>
+                <h3 class="js-animeTitle">${eachFav.title}</h3><i class="fa-regular fa-circle-xmark"></i>
+                <img src="${eachFav.images.jpg.image_url}" alt="cartel de ${eachFav.title}" class="js-animeImage">
+            </li>`;
+            console.log("fav");
+        }
+    }
+};
+
+
+
+btnSearch.addEventListener('click',getDataApi);
+
+
+
+
+
+
+
+
+
+
+
